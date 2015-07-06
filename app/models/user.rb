de28241071fec39f179d7a87ff8b8cbf2d10254a
Andrew David Burt <andrew@ampletorque.com>
2015-07-06 14:35:59 -0700

    partial add auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,15 @@
 class User < ActiveRecord::Base
+
+  enum role: [:user, :admin]
+  after_initialize :set_default_role, :if => :new_record?
+
   attr_accessor :password
   validates_confirmation_of :password
   before_save :encrypt_password
+
+  def set_default_role
+    self.role ||= :user
+  end
 
   def encrypt_password
     self.password_salt = BCrypt::Engine.generate_salt
@@ -16,5 +24,5 @@ class User < ActiveRecord::Base
       nil
     end
   end
-  
+
 end
